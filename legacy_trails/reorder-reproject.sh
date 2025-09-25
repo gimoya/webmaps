7d6d42383c11c9d2e5d1c2f920f825a4fe8ada41
Kay Cichini <kay.cichini@gmail.com>
2025-09-25 15:02:59 +0200

    latest updates

--- legacy_trails/reorder-reproject.sh
+++ legacy_trails/reorder-reproject.sh
@@ -1,15 +1,2 @@
-echo -e "\n---\n  ..reproject Legacy Trails? Hit y/n + [Enter]:"
-read yesno
-if [ "$yesno" = "y" ] 
-then
-	cd ~
-	cd C:/GitHub/legacy_trails
-	ogr2ogr my_trails_z.geojson my_trails_z_epsg3857.gpkg -s_srs EPSG:900913 -t_srs EPSG:4326
-	echo -e "\\n  ..reprojection to WGS84 done\\n---\\n"
-else 
-	echo -e "\\n  You typed "$yesno" ..part skipped..\\n---\\n"
-fi
-
-echo -e "\\n---\\n"
-read -p "Hit [Enter] to exit..."
-
+ogr2ogr my_trails_z.geojson my_trails_z_epsg3857.gpkg -s_srs EPSG:900913 -t_srs EPSG:4326
+echo -e "\\n  ..reprojection to WGS84 done\\n---\\n"
